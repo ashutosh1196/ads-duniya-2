d922aeff75811e851133347747fe587862f6b93a
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-09 18:51:37 +0530

    Login Route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/admin/dashboard';
+    public const HOME = '/admin_panel/dashboard';
 
     /**
      * The controller namespace for the application.
