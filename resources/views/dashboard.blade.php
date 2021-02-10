a69f57e759908839e0fc7ff282c67c18238cbf59
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-10 19:07:48 +0530

    Dashboard Layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,9 +12,8 @@
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
-          <!-- ./col -->
+
           <div class="col-md-3 col-lg-3 col-xl-3 col-6">
-            <!-- small box -->
             <div class="small-box customer">
               <div class="inner">
                  <div class="left">
@@ -32,49 +31,8 @@
               More Info</a>
             </div>
           </div>
-          <!-- ./col -->
+
           <div class="col-md-3 col-lg-3 col-xl-3 col-6">
-            <!-- small box -->
-            <div class="small-box">
-              <div class="inner">
-                <div class="left">
-                  <img src="{{ env('ADMIN_URL') }}images/recruiter-3.svg" class="on-hover" alt="">
-                  <img src="{{ env('ADMIN_URL') }}images/recruiter-2.svg" alt="">
-                </div>
-                <div class="right">
-                  <p>Recruiter(s)</p>
-                  <h3>{{ $recruitersCount }}</h3>
-                </div>
-              </div>
-              <a href="{{ route('recruiters_list') }}" class="small-box-footer">
-              <img src="{{ env('ADMIN_URL') }}images/next.svg" class="on-hover" alt="">
-              <img src="{{ env('ADMIN_URL') }}images/next-2.svg" alt="">
-              More Info</a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-md-3 col-lg-3 col-xl-3 col-6">
-            <!-- small box -->
-            <div class="small-box admin">
-              <div class="inner">
-                <div class="left">
-                  <img src="{{ env('ADMIN_URL') }}images/admin.svg" class="on-hover" alt="">
-                  <img src="{{ env('ADMIN_URL') }}images/user-2.svg" alt="">
-                </div>
-                <div class="right">
-                  <p>Admin(s)</p>
-                  <h3>{{ $adminsCount }}</h3>
-                </div>
-              </div>
-              <a href="{{ route('recruiters_list') }}" class="small-box-footer">
-              <img src="{{ env('ADMIN_URL') }}images/next.svg" class="on-hover" alt="">
-              <img src="{{ env('ADMIN_URL') }}images/next-2.svg" alt="">
-              More Info</a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-md-3 col-lg-3 col-xl-3 col-6">
-            <!-- small box -->
             <div class="small-box jobseeker">
               <div class="inner">
                 <div class="left">
@@ -92,8 +50,46 @@
               More Info</a>
             </div>
           </div>
+
           <div class="col-md-3 col-lg-3 col-xl-3 col-6">
-            <!-- small box -->
+            <div class="small-box">
+              <div class="inner">
+                <div class="left">
+                  <img src="{{ env('ADMIN_URL') }}images/recruiter-3.svg" class="on-hover" alt="">
+                  <img src="{{ env('ADMIN_URL') }}images/recruiter-2.svg" alt="">
+                </div>
+                <div class="right">
+                  <p>Recruiter(s)</p>
+                  <h3>{{ $recruitersCount }}</h3>
+                </div>
+              </div>
+              <a href="{{ route('recruiters_list') }}" class="small-box-footer">
+              <img src="{{ env('ADMIN_URL') }}images/next.svg" class="on-hover" alt="">
+              <img src="{{ env('ADMIN_URL') }}images/next-2.svg" alt="">
+              More Info</a>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-lg-3 col-xl-3 col-6">
+            <div class="small-box admin">
+              <div class="inner">
+                <div class="left">
+                  <img src="{{ env('ADMIN_URL') }}images/admin.svg" class="on-hover" alt="">
+                  <img src="{{ env('ADMIN_URL') }}images/user-2.svg" alt="">
+                </div>
+                <div class="right">
+                  <p>Admin(s)</p>
+                  <h3>{{ $adminsCount }}</h3>
+                </div>
+              </div>
+              <a href="{{ route('recruiters_list') }}" class="small-box-footer">
+              <img src="{{ env('ADMIN_URL') }}images/next.svg" class="on-hover" alt="">
+              <img src="{{ env('ADMIN_URL') }}images/next-2.svg" alt="">
+              More Info</a>
+            </div>
+          </div>
+
+          <!-- <div class="col-md-3 col-lg-3 col-xl-3 col-6">
             <div class="small-box job-posted">
               <div class="inner">
                 <div class="left">
@@ -111,10 +107,8 @@
               More Info</a>
             </div>
           </div>
-          <!-- ./col -->
-          <!-- ./col -->
+
           <div class="col-md-3 col-lg-3 col-xl-3 col-6">
-            <!-- small box -->
             <div class="small-box job-applied">
               <div class="inner">
                 <div class="left">
@@ -131,8 +125,8 @@
               <img src="{{ env('ADMIN_URL') }}images/next-2.svg" alt="">
               More Info</a>
             </div>
-          </div>
-          <!-- ./col -->
+          </div> -->
+
         </div>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
