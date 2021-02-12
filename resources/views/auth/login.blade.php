1daefecfb38ae32eb44b85c1f2f0b0d616d974a0
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-12 09:22:46 +0530

    Designs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,79 +18,86 @@
     @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
 @endif
 
-@section('auth_header', __('adminlte::adminlte.login_message'))
 
 @section('auth_body')
-    <form action="{{ $login_url }}" method="post">
-        {{ csrf_field() }}
-
-        {{-- Email field --}}
-        <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
-                   value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus>
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-envelope {{ config('adminlte.classes_auth_icon', '') }}"></span>
-                </div>
-            </div>
-            @if($errors->has('email'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </div>
-            @endif
+    <div class="login_wrap">
+        <div class="left">
+            <img src="http://server3.rvtechnologies.in/which-vocation/Amrik-which-vocation-admin/public/images/login-4.jpg" class="on-hover" alt="">
         </div>
-
-        {{-- Password field --}}
-        <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
-                   placeholder="{{ __('adminlte::adminlte.password') }}">
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
-                </div>
-            </div>
-            @if($errors->has('password'))
-                <div class="invalid-feedback">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </div>
-            @endif
-        </div>
-
-        {{-- Login field --}}
-        <div class="row">
-            <div class="col-7">
-                <div class="icheck-primary">
-                    <input type="checkbox" name="remember" id="remember">
-                    <label for="remember">{{ __('adminlte::adminlte.remember_me') }}</label>
-                </div>
-            </div>
-            <div class="col-5">
-                <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
-                    <span class="fas fa-sign-in-alt"></span>
-                    {{ __('adminlte::adminlte.sign_in') }}
-                </button>
-            </div>
-        </div>
-
-    </form>
-@stop
-
-@section('auth_footer')
-    {{-- Password reset link --}}
-    @if($password_reset_url)
-        <p class="my-0">
-            <a href="{{ $password_reset_url }}">
-                {{ __('adminlte::adminlte.i_forgot_my_password') }}
+        <div class="right">
+        <div class="login-logo-inner">
+            <a href="http://server3.rvtechnologies.in/which-vocation/Amrik-which-vocation-admin/public/admin_panel/dashboard">
+                <img src="http://server3.rvtechnologies.in/which-vocation/Amrik-which-vocation-admin/public/images/logo.svg" height="50">
+                <b></b>
             </a>
-        </p>
-    @endif
+            </div>
+             <div class="card-header-heading">
+                <h3 class="card-title float-none text-center">Sign In</h3>
+            </div>
+            <form action="{{ $login_url }}" method="post">
+                    {{ csrf_field() }}
 
-    {{-- Register link --}}
-    @if($register_url)
-        <p class="my-0">
-            <a href="{{ $register_url }}">
-                {{ __('adminlte::adminlte.register_a_new_membership') }}
-            </a>
-        </p>
-    @endif
+                    {{-- Email field --}}
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
+                               value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus>
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                            </div>
+                        </div>
+                        @if($errors->has('email'))
+                            <div class="invalid-feedback">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </div>
+                        @endif
+                    </div>
+
+                    {{-- Password field --}}
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
+                               placeholder="{{ __('adminlte::adminlte.password') }}">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock {{ config('adminlte.classes_auth_icon', '') }}"></span>
+                            </div>
+                        </div>
+                        @if($errors->has('password'))
+                            <div class="invalid-feedback">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </div>
+                        @endif
+                    </div>
+
+                    {{-- Login field --}}
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="icheck-primary">
+                                <input type="checkbox" name="remember" id="remember">
+                                <label for="remember">{{ __('adminlte::adminlte.remember_me') }}</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button type=submit class="btn btn-block {{ config('adminlte.classes_auth_btn', 'btn-flat btn-primary') }}">
+                                <span class="fas fa-sign-in-alt"></span>
+                                {{ __('adminlte::adminlte.sign_in') }}
+                            </button>
+                        </div>
+                    </div>
+
+                </form>
+<!--              <div class="card-footer-inner d-flex justify-content-between">                 
+                <p class="my-0">
+                    <a href="http://server3.rvtechnologies.in/which-vocation/Amrik-which-vocation-admin/public/password/reset">
+                        Forgot Password?
+                    </a>
+                </p>
+                <p class="my-0">
+                    <a href="http://server3.rvtechnologies.in/which-vocation/Amrik-which-vocation-admin/public/register">
+                        Register
+                    </a>
+                </p>
+             </div> -->
+        </div>
+    </div>
 @stop
