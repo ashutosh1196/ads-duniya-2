e08964622712bcb0889fe86ccafa2870c3ca1b6c
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-17 12:21:38 +0530

    Job Industries, Job Functions and Skills Migrations

---
+++ database/migrations/2021_02_17_064749_create_skills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->longtext('description');
+            $table->integer('status')->comment('1 => Active , 0 => Incative')->defualt(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+}
