35c6eaf97c78118be44bd8ef3c873e7efd56245a
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-17 12:26:42 +0530

    Job Functions

--- database/migrations/2021_02_17_064741_create_job_functions_table.php
+++ database/migrations/2021_02_17_064741_create_job_functions_table.php
@@ -18,6 +18,7 @@ class CreateJobFunctionsTable extends Migration
             $table->string('name');
             $table->string('slug');
             $table->longtext('description');
+            $table->foreignId('job_industry_id')->constrained();
             $table->integer('status')->comment('1 => Active , 0 => Incative')->defualt(1);
             $table->timestamps();
         });
