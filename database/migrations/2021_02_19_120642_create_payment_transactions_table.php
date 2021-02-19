a5d559e23f3332b5c2b6fab44ff98fa1cace6a14
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-19 17:44:43 +0530

    Payment Transactions Migration

---
+++ database/migrations/2021_02_19_120642_create_payment_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->float('amount')->default(0);
+            $table->foreignId('organization_id')->constrained();
+            $table->foreignId('recruiter_id')->constrained();
+            $table->string('transaction_id');
+            $table->dateTime('payment_date');
+            $table->enum('status', ['success', 'failed'] );
+            $table->longText('response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_transactions');
+    }
+}
