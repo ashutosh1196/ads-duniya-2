379c9ac4cf13637981133cbb90793d775b35f302
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-19 11:06:12 +0530

    credits

---
+++ database/migrations/2021_02_19_050845_create_organization_credits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizationCreditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_credits', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('total_credits')->default(0);
+            $table->tinyInteger('status')->comment('1 => Active , 0 => Incative')->default(1);
+            $table->foreignId('organization_id')->constrained();
+            $table->foreignId('recruiter_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization_credits');
+    }
+}
---
+++ database/migrations/2021_02_19_050919_create_organization_credit_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizationCreditDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_credit_details', function (Blueprint $table) {
+            $table->id();
+            $table->enum('txn_type',['credit','debit']);
+            $table->bigInteger('credits');
+            $table->enum('credit_type',['paid','free']);
+            $table->tinyInteger('status')->comment('1 => Active , 0 => Incative')->default(1);
+            $table->foreignId('organization_id')->constrained();
+            $table->foreignId('organization_credit_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization_credit_details');
+    }
+}
