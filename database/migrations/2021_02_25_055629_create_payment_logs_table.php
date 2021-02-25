41f0450548fd836388b209724e8ed07f994964a9
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-25 15:13:44 +0530

    qweqwe

---
+++ database/migrations/2021_02_25_055629_create_payment_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('txn_id');
+            $table->string('amount');
+            $table->string('status');
+            $table->string('description');
+            $table->text('request');
+            $table->text('response');
+            $table->foreignId('recruiter_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_logs');
+    }
+}
