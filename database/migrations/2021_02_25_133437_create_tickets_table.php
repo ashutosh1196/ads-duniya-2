f98bf76949e7ce4f597da0057ab745c8e0c9efe7
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-25 19:46:46 +0530

    qweqwe

---
+++ database/migrations/2021_02_25_133437_create_tickets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recruiter_id')->constrained();
+            $table->string('subject');
+            $table->enum('status',['open','close'])->default('open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
