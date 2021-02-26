30c50c0a8c0d1f35765a3ab2dcdf919ad4fe4336
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-02-26 17:44:03 +0530

    Div

--- resources/views/tickets/view_ticket.blade.php
+++ resources/views/tickets/view_ticket.blade.php
@@ -31,50 +31,50 @@
                     <div class="subject">
                       <label>{{ $ticket[0]->subject }}</label>
                     </div>
-                    <?php  for ($i=0; $i < count($ticketMessages); $i++) {  ?>
-                      <div class="form_wrap mb-md-5 mb-4 message_wraper">
-                        <div class="message_box">
-                          <div class="profile">
-                            <div class="user_img">
-                              <?php
-                                $publicPath = config('adminlte.website_url');
-                                $logoPath = $publicPath.'images/companyLogos/';
-                                $defauktProfileImage = $publicPath.'images/profile/default-profile-image.svg';
-                              ?>
-                              <img src="{{ $organizationLogo ? $logoPath.$organizationLogo : $defauktProfileImage }}" alt="">
-                            </div>
-                            <div class="user_detail">
-                              <?php $recruiterName = $recruiter[0]->name ? $recruiter[0]->name : 'Anonymous'; ?>
-                              <label class="name">{{ $ticketMessages[$i]->sent_by == 'admin' ? $superAdmin[0]->name : $recruiterName }}</label>
-                              <label class="time_date">{{ date('F d, Y - H:i A', strtotime($ticketMessages[$i]->created_at)) }}</label>
-                            </div>
+                    <div class="form_wrap mb-md-5 mb-4 message_wraper">
+                      <?php  for ($i=0; $i < count($ticketMessages); $i++) {  ?>
+                      <div class="message_box">
+                        <div class="profile">
+                          <div class="user_img">
+                            <?php
+                              $publicPath = config('adminlte.website_url');
+                              $logoPath = $publicPath.'images/companyLogos/';
+                              $defauktProfileImage = $publicPath.'images/profile/default-profile-image.svg';
+                            ?>
+                            <img src="{{ $organizationLogo ? $logoPath.$organizationLogo : $defauktProfileImage }}" alt="">
                           </div>
-                          <div class="message">
-                            <p>{{ $ticketMessages[$i]->message_text }}</p>
+                          <div class="user_detail">
+                            <?php $recruiterName = $recruiter[0]->name ? $recruiter[0]->name : 'Anonymous'; ?>
+                            <label class="name">{{ $ticketMessages[$i]->sent_by == 'admin' ? $superAdmin[0]->name : $recruiterName }}</label>
+                            <label class="time_date">{{ date('F d, Y - H:i A', strtotime($ticketMessages[$i]->created_at)) }}</label>
                           </div>
-                          <div class="attachment_show">
-                            <div class="box_wrap">
-                              <?php if($ticketMessages[$i]->attachment_file) {
-                                $destinationPath = config('adminlte.website_url').'ticket_images/'; ?>
-                                <img class="attached-image" src="{{ $destinationPath.$ticketMessages[$i]->attachment_file }}" alt="">
-                              <?php } ?>
-                            </div>
+                        </div>
+                        <div class="message">
+                          <p>{{ $ticketMessages[$i]->message_text }}</p>
+                        </div>
+                        <div class="attachment_show">
+                          <div class="box_wrap">
+                            <?php if($ticketMessages[$i]->attachment_file) {
+                              $destinationPath = config('adminlte.website_url').'ticket_images/'; ?>
+                              <img class="attached-image" src="{{ $destinationPath.$ticketMessages[$i]->attachment_file }}" alt="">
+                            <?php } ?>
                           </div>
                         </div>
                       </div>
                       <hr>
                       <?php } ?>
+                    </div>
                       <form action="{{ route('reply_on_ticket') }}" method="post" id="replyForm" enctype="multipart/form-data">
-                        <div class="message_reply">
-                          <div class="form-group">
-                            <textarea id="message_text" name="message_text" class="form-control"></textarea>
-                            <div class="upload-file">
-                              <input type="hidden" name="ticket_id" value="{{ $ticket[0]->id }}">
-                              <input type="file" name="attachment_file" id="attachment_file">
-                            </div>
-                            <button type="submit">Reply</button>                              
+                      <div class="message_reply">
+                        <div class="form-group">
+                          <textarea id="message_text" name="message_text" class="form-control"></textarea>
+                          <div class="upload-file">
+                            <input type="hidden" name="ticket_id" value="{{ $ticket[0]->id }}">
+                            <input type="file" name="attachment_file" id="attachment_file">
                           </div>
+                          <button type="submit">Reply</button>                              
                         </div>
+                      </div>
                     </form>                                     
                   </div>
                 </div>
