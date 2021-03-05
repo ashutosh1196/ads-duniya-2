6f490116a4e0794321f6758ef2299e19d0d76f86
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-03-05 18:11:07 +0530

    eqweqwe

--- database/seeders/AdminsSeeder.php
+++ database/seeders/AdminsSeeder.php
@@ -26,145 +26,145 @@ class AdminsSeeder extends Seeder
 		\DB::table('admins')->insert($admins);
 
 
-        \DB::table('skills')->delete();
+        // \DB::table('skills')->delete();
 
-        \DB::table('skills')->insert([
-            'name' => 'HTML',
-            'slug' => 'html',
-            'description' => 'desc',
-            'status' => 1
-        ]);
+        // \DB::table('skills')->insert([
+        //     'name' => 'HTML',
+        //     'slug' => 'html',
+        //     'description' => 'desc',
+        //     'status' => 1
+        // ]);
 
-        \DB::table('job_functions')->delete();
+        // \DB::table('job_functions')->delete();
 
-        \DB::table('job_functions')->insert([
-            'name' => 'Designer',
-            'slug' => 'designer',
-            'description' => 'desc',
-            'status' => 1
-        ]);
+        // \DB::table('job_functions')->insert([
+        //     'name' => 'Designer',
+        //     'slug' => 'designer',
+        //     'description' => 'desc',
+        //     'status' => 1
+        // ]);
 
-        \DB::table('job_locations')->delete();
+        // \DB::table('job_locations')->delete();
 
-        \DB::table('job_locations')->insert([
-            'name' => 'East London',
-            'slug' => 'EL',
-            'description' => 'desc',
-            'status' => 1
-        ]);
+        // \DB::table('job_locations')->insert([
+        //     'name' => 'East London',
+        //     'slug' => 'EL',
+        //     'description' => 'desc',
+        //     'status' => 1
+        // ]);
 
-        \DB::table('job_industries')->delete();
+        // \DB::table('job_industries')->delete();
 
-        \DB::table('job_industries')->insert([
-            'name' => 'Information Technology',
-            'slug' => 'information_technology',
-            'description' => 'desc',
-            'status' => 1
-        ]);
+        // \DB::table('job_industries')->insert([
+        //     'name' => 'Information Technology',
+        //     'slug' => 'information_technology',
+        //     'description' => 'desc',
+        //     'status' => 1
+        // ]);
 
-        \DB::table('organizations')->delete();
+        // \DB::table('organizations')->delete();
 
-        \DB::table('organizations')->insert([
-            'name' => 'RV Technologies',
-            'email' => 'info@rvtechnologies.com',
-            'country_code' => '+91',
-            'contact_number' => '+918968519881',
-            'url' => 'https://rvtechnologies.com/',
-            'domain' => 'rvtechnologies.com',
-            'is_whitelisted' => 1,
-            'address' => 'C127, Ground Floor',
-            'city' => 'Mohali',
-            'county' => 'Phase 8 Industrial Area',
-            'state' => 'Punjab',
-            'country' => 'United Kingdom',
-            'pincode' => '132654'
-        ]);    
+        // \DB::table('organizations')->insert([
+        //     'name' => 'RV Technologies',
+        //     'email' => 'info@rvtechnologies.com',
+        //     'country_code' => '+91',
+        //     'contact_number' => '+918968519881',
+        //     'url' => 'https://rvtechnologies.com/',
+        //     'domain' => 'rvtechnologies.com',
+        //     'is_whitelisted' => 1,
+        //     'address' => 'C127, Ground Floor',
+        //     'city' => 'Mohali',
+        //     'county' => 'Phase 8 Industrial Area',
+        //     'state' => 'Punjab',
+        //     'country' => 'United Kingdom',
+        //     'pincode' => '132654'
+        // ]);    
 
-        \DB::table('recruiters')->delete();
+        // \DB::table('recruiters')->delete();
 
-        \DB::table('recruiters')->insert([[
-            'email' => 'ashish_kumar@rvtechnologies.com',
-            'password' => bcrypt('12345678'),
-            'signup_via' => 'web',
-            'status' => 'active',
-            'ip_address' => '192.168.1.65',
-            'organization_id' => 1,
-            'is_parent' => 0
-        ],[
-            'email' => 'pawanjeet@rvtechnologies.com',
-            'password' => bcrypt('12345678'),
-            'signup_via' => 'web',
-            'status' => 'active',
-            'ip_address' => '192.168.1.65',
-            'organization_id' => 1,
-            'is_parent' => 0
-        ],[
-            'email' => 'sandeep@rvtechnologies.com',
-            'password' => bcrypt('12345678'),
-            'signup_via' => 'web',
-            'status' => 'active',
-            'ip_address' => '192.168.1.65',
-            'organization_id' => 1,
-            'is_parent' => 1
-        ],[
-            'email' => 'sunil_jaswal@rvtechnologies.com',
-            'password' => bcrypt('12345678'),
-            'signup_via' => 'web',
-            'status' => 'active',
-            'ip_address' => '192.168.1.65',
-            'organization_id' => 1,
-            'is_parent' => 0
-        ]]);
+        // \DB::table('recruiters')->insert([[
+        //     'email' => 'ashish_kumar@rvtechnologies.com',
+        //     'password' => bcrypt('12345678'),
+        //     'signup_via' => 'web',
+        //     'status' => 'active',
+        //     'ip_address' => '192.168.1.65',
+        //     'organization_id' => 1,
+        //     'is_parent' => 0
+        // ],[
+        //     'email' => 'pawanjeet@rvtechnologies.com',
+        //     'password' => bcrypt('12345678'),
+        //     'signup_via' => 'web',
+        //     'status' => 'active',
+        //     'ip_address' => '192.168.1.65',
+        //     'organization_id' => 1,
+        //     'is_parent' => 0
+        // ],[
+        //     'email' => 'sandeep@rvtechnologies.com',
+        //     'password' => bcrypt('12345678'),
+        //     'signup_via' => 'web',
+        //     'status' => 'active',
+        //     'ip_address' => '192.168.1.65',
+        //     'organization_id' => 1,
+        //     'is_parent' => 1
+        // ],[
+        //     'email' => 'sunil_jaswal@rvtechnologies.com',
+        //     'password' => bcrypt('12345678'),
+        //     'signup_via' => 'web',
+        //     'status' => 'active',
+        //     'ip_address' => '192.168.1.65',
+        //     'organization_id' => 1,
+        //     'is_parent' => 0
+        // ]]);
 
-        \DB::table('organization_credits')->delete();
+        // \DB::table('organization_credits')->delete();
 
-        \DB::table('organization_credits')->insert([
-            'trial_credits' => 60,
-            'organization_id' => 1,
-        ]);    
+        // \DB::table('organization_credits')->insert([
+        //     'trial_credits' => 60,
+        //     'organization_id' => 1,
+        // ]);    
 
 
-        \DB::table('organization_credit_details')->delete();
+        // \DB::table('organization_credit_details')->delete();
 
-        \DB::table('organization_credit_details')->insert([
-            'txn_type' => 'credit',
-            'credits' => '60',
-            'credit_type' => 'free',
-            'organization_id' => 1,
-            'organization_credit_id' => 1
-        ]);    
+        // \DB::table('organization_credit_details')->insert([
+        //     'txn_type' => 'credit',
+        //     'credits' => '60',
+        //     'credit_type' => 'free',
+        //     'organization_id' => 1,
+        //     'organization_credit_id' => 1
+        // ]);    
 
-        \DB::table('jobs')->insert([
-            'job_ref_number' => '2343202116148530002',
-            'job_title' => 'Designer',
-            'job_description' => 'Designer Job',
-            'job_type' => 'full_time',
-            'is_featured' => 0,
-            'job_address' => 'RV Technologies',
-            'city' => 'Mohali',
-            'county' => 'Phase 8',
-            'state' => 'Punjab',
-            'country' => 'United Kingdom',
-            'pincode' => '125687',
-            'job_url' => 'http://rvtechnologies.com',
-            'job_industry_id' => 1,
-            'job_function_id' => 1,
-            'job_location_id' => 1,
-            'package_range_from' => 120000,
-            'package_range_to' => 150000,
-            'salary_currency' => 'dollars',
-            'experience_range_min' => 1.5,
-            'experience_range_max' => 2.5,
-            'status' => 'open',
-            'recruiter_id' => 1,
-            'organization_id' => 1,
-            'expiring_at' => '2022-06-04 11:59:59'
-        ]);
+        // \DB::table('jobs')->insert([
+        //     'job_ref_number' => '2343202116148530002',
+        //     'job_title' => 'Designer',
+        //     'job_description' => 'Designer Job',
+        //     'job_type' => 'full_time',
+        //     'is_featured' => 0,
+        //     'job_address' => 'RV Technologies',
+        //     'city' => 'Mohali',
+        //     'county' => 'Phase 8',
+        //     'state' => 'Punjab',
+        //     'country' => 'United Kingdom',
+        //     'pincode' => '125687',
+        //     'job_url' => 'http://rvtechnologies.com',
+        //     'job_industry_id' => 1,
+        //     'job_function_id' => 1,
+        //     'job_location_id' => 1,
+        //     'package_range_from' => 120000,
+        //     'package_range_to' => 150000,
+        //     'salary_currency' => 'dollars',
+        //     'experience_range_min' => 1.5,
+        //     'experience_range_max' => 2.5,
+        //     'status' => 'open',
+        //     'recruiter_id' => 1,
+        //     'organization_id' => 1,
+        //     'expiring_at' => '2022-06-04 11:59:59'
+        // ]);
 
-        \DB::table('job_skill')->insert([
-            'job_id' => 1,
-            'skill_id' => 1,
-            'status' => 1
-        ]);
+        // \DB::table('job_skill')->insert([
+        //     'job_id' => 1,
+        //     'skill_id' => 1,
+        //     'status' => 1
+        // ]);
     }
 }
