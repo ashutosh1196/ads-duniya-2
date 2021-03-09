4d7d56f71e59895fc74b979305affe9490d221f9
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-03-09 10:20:35 +0530

    Email

--- app/Http/Controllers/OrganizationsController.php
+++ app/Http/Controllers/OrganizationsController.php
@@ -154,6 +154,8 @@ class OrganizationsController extends Controller {
 			$customer = Organization::where('id', $customerId)->get();
 			$random_pass = uniqid();
 			$link = config("adminlte.email_verify_url").$random_pass;
+			// $link = config("adminlte.email_verify_url").$random_pass.'?email='.$customer[0]->email;
+			// $link = config("adminlte.email_verify_url").$random_pass.'/'.$customer[0]->email;
 			$mailSent = Mail::to($customer[0]->email)->cc(['sandeep@rvtechnologies.com'])->send(new VerifyUser($customer[0]->name, $link));
 			$updateRecruiter = Recruiter::where('email', $customer[0]->email)->update(['signup_token' => $random_pass]);
 			if($updateRecruiter) {
