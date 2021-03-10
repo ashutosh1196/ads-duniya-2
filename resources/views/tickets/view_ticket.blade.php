dc1c87f68ff6d117ce209fc4cb324a0056b52004
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-03-10 18:13:04 +0530

    path

--- resources/views/tickets/view_ticket.blade.php
+++ resources/views/tickets/view_ticket.blade.php
@@ -65,7 +65,7 @@
                         <div class="attachment_show">
                           <div class="box_wrap">
                             <?php if($ticketMessages[$i]->attachment_file) {
-                              $destinationPath = config('adminlte.website_url').config('adminlte.ticket_images_path');
+                              $destinationPath = config('adminlte.website_url').'ticket_images/';
                               $extension = explode('.', $ticketMessages[$i]->attachment_file)[1];
                             ?>
                               @if($extension == 'pdf')
