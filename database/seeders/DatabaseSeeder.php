e52d040d940ad952afc2a8739ece439f54e92ec1
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-03-11 12:14:37 +0530

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-	        CitiesTableSeeder::class,
-	        CountiesTableSeeder::class,
+	        CitiesSeeder::class,
+	        CountiesSeeder::class,
 	        CountriesTableSeeder::class,
 	        DropdownsSeeder::class,
 	        JobFunctionsSeeder::class,
