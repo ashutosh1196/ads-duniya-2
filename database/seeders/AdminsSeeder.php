e0c549a574cc3f095130b24098a860bb4e0ee71a
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-03-11 11:45:34 +0530

    qwe

--- database/seeders/AdminsSeeder.php
+++ database/seeders/AdminsSeeder.php
@@ -359,5 +359,30 @@ class AdminsSeeder extends Seeder
         ];
         \DB::table('counties')->insert($counties);
 
+        \DB::table('dropdowns')->delete();
+
+        $dropdowns = [
+            ['name'=>'employment_eligibility','slug'=>'visa_considered','value'=>'Visa considered'],
+            ['name'=>'employment_eligibility','slug'=>'sponsorship_offered','value'=>'Sponsorship offered'],
+            ['name'=>'currency','slug'=>'pounds','value'=>'GBP Pound'],
+            ['name'=>'currency','slug'=>'dollars','value'=>'USD'],
+            ['name'=>'job_type','slug'=>'full_time','value'=>'Full Time'],
+            ['name'=>'job_type','slug'=>'contract_basis','value'=>'Contract Basis'],
+            ['name'=>'job_type','slug'=>'part_time','value'=>'Part Time'],
+            ['name'=>'job_type','slug'=>'graduate','value'=>'Graduate'],
+            ['name'=>'job_type','slug'=>'public_sector','value'=>'Public Sector'],
+            ['name'=>'job_type','slug'=>'work_from_home','value'=>'Work From Home'],
+        ];
+
+        \DB::table('counties')->insert($dropdowns);
     }
 }
+
+
+
+   
+  
+   
+    
+   
+  
