f38e5f577468d02fe5dd23afd8bb2925a62a3aab
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-03-18 11:31:37 +0530

    qwe

--- database/seeders/DropdownsSeeder.php
+++ database/seeders/DropdownsSeeder.php
@@ -24,6 +24,17 @@ class DropdownsSeeder extends Seeder {
 			['name'=>'job_type','slug'=>'graduate','value'=>'Graduate'],
 			['name'=>'job_type','slug'=>'public_sector','value'=>'Public Sector'],
 			['name'=>'job_type','slug'=>'work_from_home','value'=>'Work From Home'],
+
+			['name'=>'job_distance','slug'=>'5','value'=>'5 Kms'],
+			['name'=>'job_distance','slug'=>'10','value'=>'10 Kms'],
+			['name'=>'job_distance','slug'=>'25','value'=>'25 Kms'],
+			['name'=>'job_distance','slug'=>'50','value'=>'50 Kms'],
+			['name'=>'job_distance','slug'=>'100','value'=>'100 Kms'],
+
+			['name'=>'job_posted','slug'=>'7','value'=>'Within 7 days'],
+			['name'=>'job_posted','slug'=>'30','value'=>'Within 30 days'],
+			['name'=>'job_posted','slug'=>'60','value'=>'Within 60 days'],
+			['name'=>'job_posted','slug'=>'90','value'=>'Within 90 days'],
 		];
 		\DB::table('dropdowns')->insert($dropdowns);
 	}
