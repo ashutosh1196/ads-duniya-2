5959376b12672fe8f78fcb9d21e38832d7f0ace4
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-03-22 16:54:54 +0530

    zfeedback Migrations

--- database/migrations/2021_03_22_111842_create_feedback_table.php
+++ database/migrations/2021_03_22_111842_create_feedback_table.php
@@ -4,21 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFeedbacksTable extends Migration {
+class CreateFeedbackTable extends Migration {
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	*/
 	public function up() {
-		Schema::create('feedbacks', function (Blueprint $table) {
+		Schema::create('feedback', function (Blueprint $table) {
 			$table->id();
 			$table->enum('rating', [1, 2, 3, 4, 5]);
 			$table->longtext('message')->nullable();
-			$table->enum('added_by',['jobseeker', 'recruiter']);
+			$table->enum('added_by',['jobseeker', 'recruiter', 'guest']);
 			$table->foreignId('user_id')->nullable()->constrained()->default(null);
 			$table->foreignId('recruiter_id')->nullable()->constrained()->default(null);
 			$table->string('file')->nullable();
+			$table->softDeletes();
 			$table->timestamps();
 		});
 	}
@@ -29,6 +30,6 @@ class CreateFeedbacksTable extends Migration {
 	 * @return void
 	*/
 	public function down() {
-		Schema::dropIfExists('feedbacks');
+		Schema::dropIfExists('feedback');
 	}
 }
