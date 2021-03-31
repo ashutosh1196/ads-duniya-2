9d8aa3bd95ba6a72121a8933e18de67d94f5a7e9
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-03-31 17:51:48 +0530

    a

--- resources/views/jobs/bookmarked/view_bookmarked_job.blade.php
+++ resources/views/jobs/bookmarked/view_bookmarked_job.blade.php
@@ -48,7 +48,7 @@
               <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6 col-12">
                 <div class="form-group">
                   <label>{{ __('adminlte::adminlte.bookmarked_by') }}</label>
-                  <a class="link-text" href="{{ route('view_jobseeker', ['id' => $user->id]) }}"><input class="form-control" placeholder="{{ $user->first_name ? $user->first_name.' '.$user->last_name : $user->email }}" readonly>
+                  <a class="link-text" href="{{ route('view_jobseeker', ['id' => $user->id]) }}"><input class="form-control" placeholder="{{ $user->first_name ? $user->first_name.' '.$user->last_name : $user->email }}" readonly></a>
                 </div>
               </div>
               <div class="col-sm-6 col-md-6 col-lg-6 col-xl-6 col-12">
