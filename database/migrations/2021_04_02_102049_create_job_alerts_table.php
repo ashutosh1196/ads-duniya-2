7e4c67dce0ef83c1750ff993453dfe4fe9320798
Pawanjeet Kaur <pawanjeet@rvtechnologies.com>
2021-04-02 18:45:57 +0530

    job alert migartion

---
+++ database/migrations/2021_04_02_102049_create_job_alerts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_alerts', function (Blueprint $table) {
+            $table->id();
+            $table->string('keywords')->nullable();
+            $table->string('locations')->nullable();
+            $table->string('distance')->nullable();
+            $table->string('min_salary')->nullable();
+            $table->string('max_salary')->nullable();
+            $table->string('job_types')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_alerts');
+    }
+}
